1062200dfa568095deebcf17f1be939ae8347d35
migeliusIV <90527998+migeliusIV@users.noreply.github.com>
2022-01-21 19:36:17 +0300

    Add files via upload

---
+++ algorithm_of_searching_a_pipe.ino
@@ -0,0 +1,103 @@
+
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+#include "printf.h"
+
+// Hardware configuration
+
+// Set up nRF24L01 radio on SPI bus plus pins 9 & 10
+
+RF24 radio(9, 10); //Arduino UNO
+
+//
+// Channel info
+//
+
+const uint8_t num_channels = 128;
+uint8_t values[num_channels];
+
+//
+// Setup
+//
+
+void setup(void)
+{
+  //
+  // Print preamble
+  //
+
+  Serial.begin(9600);
+  Serial.println("Scanner Air On");
+  printf_begin();
+
+  //
+  // Setup and configure rf radio
+  //
+
+  radio.begin();
+  radio.setAutoAck(false);
+
+  // Get into standby mode
+  radio.startListening();
+  radio.printDetails();  
+  delay(5000);              
+
+  // Print out header, high then low digit
+  int i = 0;
+  while ( i < num_channels )
+  {
+    printf("%x", i >> 4);
+    ++i;
+  }
+  printf("\n\r");
+  i = 0;
+  while ( i < num_channels )
+  {
+    printf("%x", i & 0xf);
+    ++i;
+  }
+  printf("\n\r");
+}
+
+//
+// Loop
+//
+
+const int num_reps = 100;
+
+void loop(void)
+{
+  // Clear measurement values
+  memset(values, 0, sizeof(values));
+
+  // Scan all channels num_reps times
+  int rep_counter = num_reps;
+  while (rep_counter--)
+  {
+    int i = num_channels;
+    while (i--)
+    {
+      // Select this channel
+      radio.setChannel(i);
+
+      // Listen for a little
+      radio.startListening();
+      delayMicroseconds(512);
+      radio.stopListening();
+
+      // Did we get a carrier?
+      if ( radio.testCarrier() )
+        ++values[i];
+    }
+  }
+
+  // Print out channel measurements, clamped to a single hex digit
+  int i = 0;
+  while ( i < num_channels )
+  {
+    printf("%x", min(0xf, values[i] & 0xf));
+    ++i;
+  }
+  printf("\n\r");
+}
