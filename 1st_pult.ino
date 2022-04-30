21094492dac952add074f6ae9a7573b3dcb07399
migeliusIV <90527998+migeliusIV@users.noreply.github.com>
2022-04-30 22:54:07 +0300

    Update 1st_pult.ino

--- 1st_pult.ino
+++ 1st_pult.ino
@@ -2,44 +2,53 @@
 #include <nRF24L01.h> // Подключаем файл конфигурации из библиотеки RF24
 #include <RF24.h> // Подключаем библиотеку для работа для работы с модулем NRF24L01
 
-#define PIN_POT A1  // номера пинов Arduino, к которым подключены потенциометры
-#define pin_secpot A3
-#define pin_btn 2   //кнопки
-#define pin_zapas 4
+#define pin_pot_1 A1  // Джойстик 1
+#define pin_pot_2 A3  // Джойстик 2
+#define pin_btn_A 2   // Направление 1
+#define pin_btn_B 3   // <...> Не назначена
+#define pin_btn_C 4   // Направление 2
+#define pin_btn_D 7   // Выстрел
+#define pin_btn_E 8   // slow motion, на версиях младше 2022 поменяйте пины кнопок, которые будут снаружи пульта
 #define PIN_CE  9  //  пина Arduino, к которому подключен вывод CE радиомодуля
 #define PIN_CSN 10 // Номер пина Arduino, к которому подключен вывод CSN радиомодуля
 
 RF24 radio(PIN_CE, PIN_CSN); //(пульт, так что 9 <-> 10) Создаём объект radio с указанием выводов CE и CSN
 
-int potValue[4]; // Создаём массив для передачи значений потенциометра
-
+int potValue[6]; // Создаём массив для передачи значений потенциометра и кнопок
 
 void setup() {
   //инициализация пинов кнопок
-  pinMode(pin_btn,INPUT);
-  pinMode(pin_zapas,INPUT);
-  
+  pinMode(pin_btn_A,INPUT);
+  pinMode(pin_btn_C,INPUT);
+  pinMode(pin_btn_D,INPUT);
+  pinMode(pin_btn_E,INPUT);
   //настройка монитора порта, чтобы отслеживать, на каком этапе ошибка
   Serial.begin(9600);
   
   //настройка радио
   radio.begin();  // Инициализация модуля NRF24L01
-  radio.setChannel(5); // Обмен данными будет вестись на пятом канале (2,405 ГГц)
+  radio.setChannel(10); // Обмен данными будет вестись на 16 канале (2,416 ГГц). Это объясняется тем, что каналы кодируются не в десятеричной системе счисления
   radio.setDataRate (RF24_1MBPS); // Скорость обмена данными 1 Мбит/сек
   radio.setPALevel(RF24_PA_HIGH); // Выбираем высокую мощность передатчика (-6dBm)
-  radio.openWritingPipe(0x7878787878LL); // Открываем трубу с уникальным ID
+  radio.openWritingPipe(0x6868686868LL); // Открываем трубу с уникальным ID
 }
 
 
 void loop() {
   //считывание 
-  potValue[0] = analogRead(PIN_POT)/4; // Считываем показания потенциометра
-  potValue[1] = analogRead(pin_secpot)/4;
-  potValue[2] = digitalRead(pin_btn);
-  potValue[3] = digitalRead(pin_zapas);
+  potValue[0] = analogRead(pin_pot_1)/4; // Считываем показания потенциометра
+  potValue[1] = analogRead(pin_pot_2)/4;
+  potValue[2] = digitalRead(pin_btn_A);
+  potValue[3] = digitalRead(pin_btn_C);
+  potValue[4] = digitalRead(pin_btn_D); // Кнопка выстрела
+  potValue[5] = digitalRead(pin_btn_E);
   
-  Serial.println(potValue[0]);
-  Serial.println(potValue[1]);
+  Serial.print(potValue[2]);
+  Serial.print(' ');
+  Serial.print(potValue[3]);
+  Serial.print(' ');
+  Serial.println(potValue[5]);
+
   
   radio.write(potValue, sizeof(potValue)); // Отправляем считанные показания по радиоканалу
 }
